4eac4ae1b4b8cabc77ad08860f2f3f2336a6939a
SmrutiBiswal1 <smrutiranjanbiswal1@gmail.com>
2026-03-16 16:16:00 +0530

    PlaywrightDemo

---
+++ tests/sample.spec.js
@@ -0,0 +1,30 @@
+const {test,expect} = require('@playwright/test')
+
+test('My first test', async function({page}){
+
+expect(12).toBe(12)
+
+})
+
+test("My second test",async function ({page}) {
+
+expect(100).toBe(101)
+
+})
+
+test("My third test",async function ({page}) {
+
+expect(2.0).toBe(2.0)
+
+})
+
+test("My fourth test",async function ({page}) {
+    expect("Mukesh Otwani").toContain("Mukesh")
+    expect(true).toBeTruthy()
+  })
+
+test("My fifth test",async function ({page}) {
+    expect(false).toBeFalsy()
+})
+
+
